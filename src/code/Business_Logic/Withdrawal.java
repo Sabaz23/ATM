c1575269864c8d5bd50bc28dc7e981aca506f97c
Lorenzo Contino <contino424@gmail.com>
2022-11-19 17:29:51 +0100

    Moficate classi con utilizzo di metodi di Euro

--- src/code/Business_Logic/Withdrawal.java
+++ src/code/Business_Logic/Withdrawal.java
@@ -53,7 +53,7 @@ public class Withdrawal extends Transaction
                bankDatabase.getAvailableBalance( getAccountNumber() );
       
             // check whether the user has enough money in the account 
-            if ( amount <= availableBalance.getValore() )
+            if ( !availableBalance.minoreDi(new Euro(amount)))
             {   
                // check whether the cash dispenser has enough money
                if ( cashDispenser.isSufficientCashAvailable( amount ) )
